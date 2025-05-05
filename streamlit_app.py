4b8ef8b50bd07df96f992bcc9bf74d45ebbe539b
Diana Tuleva <140511592+dtuleva@users.noreply.github.com>
2025-05-05 09:28:26 +0300

    Update streamlit_app.py
    Add nutrition df

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,7 +30,15 @@ ingredients_list = st.multiselect(
 
 if ingredients_list:
     
-    ingredients_string = " ".join(ingredients_list) + " "
+    ingredients_string = " "
+
+    for fruit_chosen in ingredients_list:
+        ingredients_string += fruit_chosen + " "
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+
+        #st.text(smoothiefroot_response.json())
+        sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
+
 
     # st.write(ingredients_string)
     
@@ -46,13 +54,6 @@ if ingredients_list:
         session.sql(my_insert_stmt).collect()
         st.success(f'Your Smoothie is ordered, {name_on_order}!', icon="✅")
 
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response)
-
-    
 
 
-
-
-
-
+  
